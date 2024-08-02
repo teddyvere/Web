e147f5ce0c794b7b1ac6cc35c255979651825228
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-08-02 11:53:44 -0400

    HTML added

---
+++ website/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask, g
+from flask_login import current_user, LoginManager
+
+
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'new_password'
+    
+    from .views import views
+    from .auth import auth
+    from .models import User  # Import the User model
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
+    def inject_user():
+        return dict(user=current_user)
+    
+    app.context_processor(inject_user)
+    
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    
+    return app
+
---
+++ website/models.py
@@ -0,0 +1,10 @@
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Note():
+    pass
+
+
+class User(UserMixin):
+    pass
