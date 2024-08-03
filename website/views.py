9f033bce2520b972ca02b9158c212a3d2c3cb481
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-08-03 12:55:14 -0400

    mesage deletion added

--- website/views.py
+++ website/views.py
@@ -1,9 +1,37 @@
-from flask import Blueprint, render_template 
+from flask import Blueprint, json, jsonify, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
+
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET','POST'])
 @login_required
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        note = request.form.get('note')
+        
+        if len(note) < 1:
+            flash('Note is too short', category='error')
+        else:
+            
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added successfully!', category='success')
+    
+    notes = current_user.notes  # Fetch the notes for the current user    
+    return render_template("home.html", user=current_user, notes=notes)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    
+    return jsonify({})
